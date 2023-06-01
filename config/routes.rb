888426d66a47fa80bacc706642d2a10c4faa62f4
Rida Batool <ridabatool.be13@gmail.com>
2023-06-01 18:28:04 -0400

    create admin-categories feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  # namespace :admin do
+  #   get 'categories/index'
+  #   get 'categories/new'
+  #   get 'categories/create'
+  # end
   root to: 'products#index'
   get 'about/', to: 'about#index'
 
@@ -13,11 +18,10 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
-  
-
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
